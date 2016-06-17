2a63b5ab3506fc7205e1368e1b44aa1ae7b6f615
claudemiro <dimiro1@gmail.com>
2016-06-16 21:52:12 -0300

    First version

--- README.md
+++ README.md
@@ -1,2 +1,37 @@
 # lambda-wkhtmltopdf
 Convert HTML to PDF using Webkit (QtWebKit) on AWS Lambda
+
+## Input
+
+```json
+{
+    "data" : "<h1>Claudemiro</h1>",
+    "filename": "optional filename",
+    "pagesize": "optional pagesize default: a4"
+}
+```
+
+## Output
+
+```json
+{
+    "filename": "8rqj9td0pvjf9a4i.pdf"
+}
+```
+
+## Configuration
+
+1. Open `config.js` and set `bucket` variable to name of S3 bucket where you want function to save output PDF files.
+2. Make sure AWS Lambda function has `PutObject` access to S3 bucket
+3. Download the binary wkhtmltopdf for linux-x64 and put in the current directory
+4. Run npm install
+5. Zip everything and upload to aws-lambda
+
+## Links
+
+* http://docs.aws.amazon.com/apigateway/latest/developerguide/api-gateway-mapping-template-reference.html
+* http://aws.amazon.com/documentation/apigateway/
+* http://wkhtmltopdf.org/
+* https://www.npmjs.com/package/wkhtmltopdf
+* http://swagger.io/
+* https://github.com/ashiina/lambda-local
---
+++ index.js
@@ -0,0 +1,71 @@
+// The MIT License (MIT)
+
+// Copyright (c) 2016 Claudemiro
+
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+const config = require('./config.js');
+
+const wkhtmltopdf = require('wkhtmltopdf');
+const fs = require('fs');
+const AWS = require('aws-sdk');
+
+const logger = config.logger;
+const S3 = new AWS.S3();
+
+// handler handle eventes in the following format
+// 
+// {
+//     "data" : "<h1>Claudemiro</h1>",
+//     "filename": "optional filename",
+//     "pagesize": "optional pagesize default: a4"
+// }
+exports.handler = function handler(event, context, callback) {
+    if (!event.data) {
+        logger.log().error('unable to get the data');
+        callback('unable to get the data', {});
+        return;
+    }
+
+    const filename = `${(event.filename || Math.random().toString(36).slice(2))}.pdf`;
+    const pageSize = event.pagesize || 'a4';
+    const data = event.data;
+
+    logger.log({ data, pageSize, filename }).info('Variables');
+
+    const output = `/tmp/${filename}`;
+    const writeStream = fs.createWriteStream(output);
+
+    wkhtmltopdf(data, { pageSize }, () => {
+        S3.putObject({
+            Bucket: config.bucket,
+            Key: filename,
+            Body: fs.createReadStream(output),
+            ContentType: 'application/pdf',
+        }, (error) => {
+            if (error != null) {
+                logger.log({ error }).error('Unable to send file to S3');
+                callback('Unable to send file to S3', {});
+            } else {
+                logger.log({ filename }).info('Upload done!');
+                callback(null, { filename });
+            }
+        });
+    }).pipe(writeStream);
+};
